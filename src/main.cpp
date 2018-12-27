19e59f9a10ba661d9cc5b903011fa4fd493ab49f
Mahmoud-Selim <mahmoudahmedselim96@gmail.com>
2018-12-27 05:42:20 +0200

    Code: Adding the final code for running the project

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,16 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
-
+#include "spline.h"
+#include <limits>
+#include <string>
+//#include "PathPlanner.h"
 using namespace std;
 
+#define LANE_WIDTH 4
+#define INEFFICIENCY_COST 10
+#define DISTANCE_COST 100
+#define SAFETY_COST 100
 // for convenience
 using json = nlohmann::json;
 
@@ -163,6 +170,153 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
 }
 
+struct lane_speed_sort
+{
+    template<class T>
+    bool operator()(T const &a, T const &b) const { return a[0] < b[0]; }
+};
+
+double lane_speed(int s, vector<vector <double>> sensor_fusion, int lane)
+{
+	int max_distance_limit = 50;
+	vector<vector<double>> lane_speeds;
+	for(auto &object: sensor_fusion)
+	{
+		double d = object[6];
+		if(d < 2 + 4*lane + 2.00 && d > 2 + 4 * lane - 2.00)
+		{
+			double vx = object[3];
+			double vy = object[4];
+			double check_speed = sqrt(vx*vx + vy*vy);
+			lane_speeds.push_back({object[5], check_speed});
+		}
+	}
+	sort(lane_speeds.begin(), lane_speeds.end(), lane_speed_sort());
+	for(auto &speed: lane_speeds)
+	{
+		if(speed[0] > s && speed[0] < s + max_distance_limit)
+		{
+			return speed[1];
+		}
+	}
+	return 50;
+}
+
+double inefficiency_cost(int s, vector<vector <double>> sensor_fusion, int lane)
+{
+	double speed = lane_speed(s, sensor_fusion, lane);
+	return 1 - exp(-1 * abs((double)speed - 50));
+}
+
+int get_lane(double d)
+{
+	return (int)(d / 4);
+}
+
+vector<int> possible_states(int lane) {
+    /*
+    Provides the possible next states given the current state for the FSM
+	the car has 3 states representing 3 lanes on the road.
+    */
+    vector<int> states;
+    states.push_back(lane);
+    //string state = this->state;
+    if(lane == 0)
+    {
+    	states.push_back(1);
+    }
+    else if(lane == 1)
+	{
+    	states.push_back(0);
+    	states.push_back(2);
+	}
+    else
+    {
+    	states.push_back(1);
+    }
+
+    return states;
+}
+
+vector<double> make_predictions(int prev_size, double car_s, vector<vector <double>> sensor_fusion)
+{
+	vector<double>predictions;
+	for(auto &object : sensor_fusion)
+	{
+		double vx = object[3];
+		double vy = object[4];
+		double check_speed = sqrt(vx*vx + vy*vy);
+		double check_car_s = object[5];
+
+		check_car_s += ((double)prev_size * 0.02 * check_speed);
+		predictions.push_back(check_car_s);
+	}
+	return predictions;
+}
+
+double distance_cost(int s, vector<vector <double>> sensor_fusion, vector <double> predictions,int lane)
+{
+	int max_distance_limit = 100;
+	vector<double> lane_distances;
+	for(size_t i = 0; i < sensor_fusion.size(); ++i)
+	{
+		double d = sensor_fusion[i][6];
+		if(d < 2 + 4*lane + 2.00 && d > 2 + 4 * lane - 2.00)
+		{
+			lane_distances.push_back(predictions[i]);
+		}
+	}
+	for(auto &distance: lane_distances)
+	{
+		if(abs(distance  - s ) < 20)
+		{
+			double cost = 1;
+			return cost;
+		}
+	}
+	return 0;
+}
+double calculate_cost(double car_s, int state, vector<double> predictions, vector<vector <double>> sensor_fusion)
+{
+	double cost = 0;
+	vector<double> costs;
+	vector<double> weights = {INEFFICIENCY_COST, DISTANCE_COST};
+
+	double ineff_cost = inefficiency_cost(car_s, sensor_fusion, state);
+	costs.push_back(ineff_cost);
+
+	double dist_cost = distance_cost(car_s,sensor_fusion, predictions, state);
+	costs.push_back(dist_cost);
+
+	for(size_t i = 0; i < costs.size(); ++i)
+	{
+		cost += (double)weights[i] * costs[i];
+	}
+	return cost;
+}
+
+
+double choose_next_state(int lane, int prev_size, double car_s, vector<vector <double>> sensor_fusion)
+{
+
+	vector<int> states = possible_states(lane);
+    vector<double> predictions = make_predictions(prev_size, car_s, sensor_fusion);
+    double cost = std::numeric_limits<double>::max();
+    int selected_state = lane;
+
+    for(auto &state : states)
+    {
+    	double state_cost = calculate_cost(car_s, state , predictions, sensor_fusion);
+        if(cost - state_cost > 2 && state_cost < 10)
+        {
+        	cost = state_cost;
+            selected_state = state;
+        }
+    }
+    return selected_state;
+}
+
+
 int main() {
   uWS::Hub h;
 
@@ -199,14 +353,15 @@ int main() {
   	map_waypoints_dx.push_back(d_x);
   	map_waypoints_dy.push_back(d_y);
   }
-
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  double ref_vel = 0; //mph
+  h.onMessage([&ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
     //auto sdata = string(data).substr(0, length);
     //cout << sdata << endl;
+
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
 
       auto s = hasData(data);
@@ -243,7 +398,146 @@ int main() {
           	vector<double> next_y_vals;
 
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+          	// define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+          	double dist_inc = 0.3;
+        	int lane;// = 1;
+        	int prev_size = previous_path_x.size();
+        	bool too_close = false;
+        	if(prev_size > 0)
+        	{
+        		car_s = end_path_s;
+        	}
+        	lane = choose_next_state(get_lane(car_d), prev_size, car_s, sensor_fusion);
+
+        	for(auto &object : sensor_fusion)
+        	{
+        		double d = object[6];
+        		if(d < 2 + 4*lane + 2.05 && d > 2 + 4 * lane - 1.95)
+        		{
+        			double vx = object[3];
+        			double vy = object[4];
+        			double check_speed = sqrt(vx*vx + vy*vy);
+        			double check_car_s = object[5];
+
+        			check_car_s += ((double)prev_size * 0.02 * check_speed);
+
+        			if(check_car_s > car_s && (check_car_s - car_s) < 30)
+        			{
+        				too_close = true;
+        			}
+        		}
+        	}
+        	if(too_close)
+        	{
+        		ref_vel -= 0.5;
+        	}
+        	else if(ref_vel < 48.5)
+        	{
+        		ref_vel += 1;
+        		too_close = false;
+        	}
+
+        	vector<double> ptsx;
+        	vector<double> ptsy;
+
+        	double ref_x = car_x;
+        	double ref_y = car_y;
+        	double ref_yaw = deg2rad(car_yaw);
+
+        	if ( prev_size < 2 )
+        	{
+        	    double prev_car_x = car_x - cos(car_yaw);
+        	    double prev_car_y = car_y - sin(car_yaw);
+
+        	    ptsx.push_back(prev_car_x);
+        	    ptsx.push_back(car_x);
+
+        	    ptsy.push_back(prev_car_y);
+        	    ptsy.push_back(car_y);
+        	}
+        	else
+        	{
+        		ref_x = previous_path_x[prev_size - 1];
+        	    ref_y = previous_path_y[prev_size - 1];
+
+        	    double ref_x_prev = previous_path_x[prev_size - 2];
+        	    double ref_y_prev = previous_path_y[prev_size - 2];
+        	    ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+        	    ptsx.push_back(ref_x_prev);
+        	    ptsx.push_back(ref_x);
+
+        	    ptsy.push_back(ref_y_prev);
+        	    ptsy.push_back(ref_y);
+        	}
+
+
+
+            vector<double> next_wp0 = getXY(car_s+30, LANE_WIDTH/2 + (LANE_WIDTH * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp1 = getXY(car_s+60, LANE_WIDTH/2 + (LANE_WIDTH * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s+90, LANE_WIDTH/2 + (LANE_WIDTH * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+            ptsx.push_back(next_wp0[0]);
+            ptsx.push_back(next_wp1[0]);
+            ptsx.push_back(next_wp2[0]);
+
+            ptsy.push_back(next_wp0[1]);
+            ptsy.push_back(next_wp1[1]);
+            ptsy.push_back(next_wp2[1]);
+
+            for (size_t i = 0; i < ptsx.size(); i++ )
+            {
+                double shift_x = ptsx[i] - ref_x;
+                double shift_y = ptsy[i] - ref_y;
+
+                ptsx[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
+                ptsy[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
+            }
+
+
+            // Create the spline.
+            tk::spline s;
+            s.set_points(ptsx, ptsy);
+
+            next_x_vals.clear();
+            next_y_vals.clear();
+
+            for ( int i = 0; i < prev_size; i++ ) {
+                next_x_vals.push_back(previous_path_x[i]);
+                next_y_vals.push_back(previous_path_y[i]);
+            }
+
+            // Calculate distance y position on 30 m ahead.
+            double target_x = 30.0;
+            double target_y = s(target_x);
+            double target_dist = sqrt(target_x*target_x + target_y*target_y);
+
+            double x_add_on = 0;
+
+            for( int i = 1; i < 50 - prev_size; i++ ) {
+
+                // 2.24 constant is to convert miles per hours to meter per seconds
+                double N = target_dist/(0.02*ref_vel/2.24);
+                double x_point = x_add_on + target_x/N;
+                double y_point = s(x_point);
+
+                x_add_on = x_point;
+
+                double x_ref = x_point;
+                double y_ref = y_point;
+
+                // rotate back to normal after rotating it earlier
+                x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+                y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+                x_point += ref_x;
+                y_point += ref_y;
+
+                next_x_vals.push_back(x_point);
+                next_y_vals.push_back(y_point);
+            }
+
+          	// END OF MY CODE.
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
@@ -293,4 +587,5 @@ int main() {
     return -1;
   }
   h.run();
+
 }
